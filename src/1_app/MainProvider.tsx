c22380051c823c389f79856438c15fdb2bfd1d8b
nnder <asfahaikrkt6k56@yandex.ru>
2024-04-28 23:16:15 +0500

    create main provider(mui theme, tanstack)

---
+++ src/1_app/MainProvider.tsx
@@ -0,0 +1,44 @@
+import {ThemeProvider, createTheme } from "@mui/material"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PropsWithChildren } from "react"
+import { Toaster } from "react-hot-toast";
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#201E1F",
+      },
+      secondary: {
+        main: "#FEEFDD",
+      },
+      info: {
+        main: "#FF4000"
+      },
+    },
+    components: {
+      // MuiContainer: {
+      //   defaultProps: {
+      //     sx: {
+      //       pt: 8
+      //     }
+      //   }
+      // },
+    }
+  })
+
+const queryClient = new QueryClient()
+
+export default function MainProvider({children}: PropsWithChildren) {
+  return (
+    <>
+      <Toaster/>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          {children}
+        </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,17 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import MainProvider from './1_app/MainProvider.tsx';
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <MainProvider>
+      <App />
+    </MainProvider>
   </React.StrictMode>,
 )
